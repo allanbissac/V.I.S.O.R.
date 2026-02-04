28f9c67a98d5f78599b77a24cc6e05cbe31db259
Yinfeng Li <li15653530008@gmail.com>
2026-02-04 15:22:12 +0000

    Add files via upload

---
+++ src/visor/manipulator/src/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'grasp_new'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='student27',
+    maintainer_email='student27@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'commander = grasp_new.commander:main',
+            'arm_worker = grasp_new.arm_worker:main',
+        ],
+    },
+)
